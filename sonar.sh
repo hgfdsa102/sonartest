d9a2f22004cf6866b121a4a6c6a0546db9e81d99
hgfdsa102 <hgfdsa102@naver.com>
2023-03-14 02:24:44 +0900

    1

--- sonar.sh
+++ sonar.sh
@@ -1,1 +1,6 @@
-if [[ $DRONE_PULL_REQUEST_TITLE == *SONAR* ]]; then echo "Start Scanning"; else echo "Pass scanning"; fi
+if [[ $DRONE_PULL_REQUEST_TITLE == *SONAR* ]]
+then
+    echo "Start Scanning"
+else
+    echo "Pass scanning"
+fi
